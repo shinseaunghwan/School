d5b545b003554b436d5ee0e131acc2af0c72d3e2
shin <tmdghkstla1@naver.com>
2025-02-06 09:50:00 +0900

    school

--- app/(main)/main/page.tsx
+++ app/(main)/main/page.tsx
@@ -1,19 +1,22 @@
-import { Metadata } from "next";
-import React from 'react';
-import App from './App';
-import Header from './Header';
-import Footer from './Footer';
+import App from "./App";
+// import Header from "./Header";
+import Footer from "./Footer";
+import MyComponent from './MyComponent.client';
 
-export const metadata: Metadata = {
-    title: "학교통합",
-  };
 
-export default function MainPage () {
-    return (
-      <>
-        <Header />
+export const metadata = {
+  title: "학교통합",
+};
+
+export default function MainPage() {
+  return (
+    <div>
+      <MyComponent />
         <App />
         <Footer />
-      </>
-    );
+    </div>
+  );
 }
+
+
+
