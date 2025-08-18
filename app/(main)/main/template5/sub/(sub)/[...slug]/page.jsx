74f1ee3a076e9f94d1d81a1b2435ff71c92d8018
shin <tmdghkstla1@naver.com>
2025-08-18 17:53:28 +0900

    ed

--- app/(main)/main/template5/sub/(sub)/[...slug]/page.jsx
+++ app/(main)/main/template5/sub/(sub)/[...slug]/page.jsx
@@ -3,36 +3,34 @@
 import { useEffect, useState } from 'react';
 import { notFound } from 'next/navigation';
 
-
-const componentPromises = {};
-for (let i = 1; i <= 100; i++) {
-  componentPromises[`cont${i}`] = import(`../(subPage)/Cont${i}`);
-}
-
 export default function DynamicSubPage({ params }) {
   const [ComponentToRender, setComponentToRender] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const slug = params.slug[0];
 
-    const componentPromise = componentPromises[slug];
-
-    if (componentPromise) {
-
-      componentPromise
-        .then(mod => {
-          setComponentToRender(() => mod.default);
-        })
-        .catch(() => {
-          notFound();
-        });
-    } else {
-      notFound();
-    }
+    // 필요한 Cont 컴포넌트를 동적으로 import
+    import(`../(subPage)/${slug.charAt(0).toUpperCase() + slug.slice(1)}`)
+      .then(mod => {
+        setComponentToRender(() => mod.default);
+      })
+      .catch(() => {
+        // 해당 컴포넌트를 찾지 못했거나 로딩에 실패한 경우
+        notFound();
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+      
   }, [params.slug]);
 
+  if (loading) {
+    return <div style={{minHeight:"600px", display:"flex", justifyContent:"center", alignItems:"center"}}>Loading...</div>; // 로딩 상태를 보여주는 UI
+  }
+
   if (!ComponentToRender) {
-    return null; 
+    return <div style={{minHeight:"600px", display:"flex", justifyContent:"center", alignItems:"center"}}>서브페이지</div>;
   }
 
   return <ComponentToRender />;
